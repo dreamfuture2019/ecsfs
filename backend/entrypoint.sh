390a0ab18f443c11fba010f293d79b4ba2972108
dreamfuture2019 <47684365+dreamfuture2019@users.noreply.github.com>
2019-09-21 20:48:03 +0700

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #curl 169.254.170.2/latest/meta-data
 curl 169.254.170.2/v2/metadata
-export ECS_INSTANCE_IP_ADDRESS=$(curl — retry 5 — connect-timeout 3 -s 169.254.170.2/latest/meta-data/local-ipv4)
-export ECS_TASK_NON_SECURE_INSTANCE_PORT=$(cat ${ECS_CONTAINER_METADATA_FILE} | grep HostPort | awk -F ":" '{print $2}' | sed 's/,$//g')
+#export ECS_INSTANCE_IP_ADDRESS=$(curl — retry 5 — connect-timeout 3 -s 169.254.170.2/latest/meta-data/local-ipv4)
+#export ECS_TASK_NON_SECURE_INSTANCE_PORT=$(cat ${ECS_CONTAINER_METADATA_FILE} | grep HostPort | awk -F ":" '{print $2}' | sed 's/,$//g')
 
-
+echo $server.port
 echo $ECS_INSTANCE_IP_ADDRESS
 echo $ECS_TASK_NON_SECURE_INSTANCE_PORT
